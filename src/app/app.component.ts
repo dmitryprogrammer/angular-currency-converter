eb8c8247bc7bdcdabde96a30ec265a92251c1626
dmitryprogrammer <dpischalka@gmail.com>
2024-11-23 00:51:32 +0300

    [signals-play] work with signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-currency-converter';
+  multiplyControl = new FormControl();
+
+  count = signal(0);
+  countMultiplied = computed(() => this.count() * this.multiplyControl?.value);
+
+  increment() {
+    this.count.set(this.count() + 1);
+  }
+
+  reset() {
+    this.count.set(0);
+    this.multiplyControl.reset();
+  }
 }
